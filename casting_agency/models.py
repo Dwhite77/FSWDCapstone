9766c29e0c2ec451cd099bf03aa107ffbfd5b96b
Dwhite77 <90846232+Dwhite77@users.noreply.github.com>
2024-10-03 09:23:29 +0100

    Update models.py
    adding extra to the models file

--- casting_agency/models.py
+++ casting_agency/models.py
@@ -2,19 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     release_date = db.Column(db.Date, nullable=False)
 
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
 
 class Actor(db.Model):
     __tablename__ = 'actors'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def __repr__(self):
+        return f'<Actor {self.name}>'
